f20ce95d27e6f736be38d76454a1f0002406a1df
Ayman Geneidy <Ayman Geneidy@AGeneidy>
2014-04-10 20:17:25 +0200

    Full buffer assignment

---
+++ MiniBase/src/heap/Heapfile.java
@@ -0,0 +1,41 @@
+package heap;
+
+import global.RID;
+
+public class Heapfile {
+
+	public Heapfile(String string) {
+		// TODO Auto-generated constructor stub
+	}
+
+	public RID insertRecord(byte[] byteArray) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public int getRecCnt() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public Scan openScan() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public boolean deleteRecord(RID rid) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public boolean updateRecord(RID rid, Tuple newTuple) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	public Tuple getRecord(RID rid) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
